37e43aae910b2493812073e309c7490e5a7bbb99
Richard Price <mrspondooli@gmail.com>
2020-10-07 15:58:58 +0100

    reformatted

--- MumsLunch/CompositionalSections/TitleSection.swift
+++ MumsLunch/CompositionalSections/TitleSection.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class TitleSection: Section {
     var numberOfitems = 1
-    
     func layoutSection() -> NSCollectionLayoutSection {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
         let items = NSCollectionLayoutItem(layoutSize: itemSize)
@@ -19,18 +18,14 @@ class TitleSection: Section {
         section.orthogonalScrollingBehavior = .groupPaging
         return section
     }
-    
     func configureCell(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: RecommenedCustomCell.self ), for: indexPath) as! RecommenedCustomCell
         cell.set(title: title)
         return cell
     }
-    
     let numberOfItems = 1
     private let title: String
     private let isShowAllHidden: Bool
-    
-    
     init(title: String, isShowAllHidden: Bool) {
         self.title = title
         self.isShowAllHidden = isShowAllHidden
--- MumsLunch/Controller/RecommendedCollectionViewController.swift
+++ MumsLunch/Controller/RecommendedCollectionViewController.swift
@@ -8,57 +8,49 @@
 import UIKit
 
 class RecommendedCollectionViewController: UICollectionViewController {
-    
-    lazy var sections: [Section] = [
+    let sections: [Section] = [
         TitleSection(title: "Recommended", isShowAllHidden: true),
         TitleSection(title: "Popular", isShowAllHidden: true),
         TitleSection(title: "Favourites", isShowAllHidden: true),
-
     ]
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .purple
-        collectionView.collectionViewLayout = titleSetup()
-        collectionView.register(RecommenedCustomCell.self, forCellWithReuseIdentifier: String(describing: RecommenedCustomCell.self))
+        setupCollectionView()
     }
-    
     init() {
         super.init(collectionViewLayout: UICollectionViewLayout())
     }
-    
-    private func titleSetup() -> UICollectionViewCompositionalLayout {
+    private func setupCollectionView() {
+        collectionView.backgroundColor = .purple
+        collectionView.collectionViewLayout = titleLableSetup()
+        collectionView.register(RecommenedCustomCell.self, forCellWithReuseIdentifier: String(describing: RecommenedCustomCell.self))
+    }
+    private func titleLableSetup() -> UICollectionViewCompositionalLayout {
         let sections = self.sections
         let layout = UICollectionViewCompositionalLayout {(sectionIndex, enviroment) -> NSCollectionLayoutSection? in
             return sections[sectionIndex].layoutSection()
         }
         return layout
     }
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         collectionView.reloadData()
     }
-
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         collectionView.reloadData()
     }
-    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         sections[section].numberOfitems
     }
-    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         sections.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         sections[indexPath.section].configureCell(collectionView: collectionView, indexPath: indexPath)
-
     }
-    
     required init?(coder: NSCoder) {
         fatalError("Not using storyboards for project")
-
+        
     }
 }
